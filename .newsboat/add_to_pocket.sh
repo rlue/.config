cd2297bed692fb3dc7e2ef83bec064cf34a6862d
Ryan Lue <hello@ryanlue.com>
2019-05-07 14:59:45 +0800

    Update newsboat's bookmark-cmd for version 2.10

--- .newsboat/add_to_pocket.sh
+++ .newsboat/add_to_pocket.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # shellcheck disable=SC2069
 
+# newsboat’s bookmark-cmd
+
 # url="$1"
 # title="$2"
 # description="$3"
+# current_feed="$4"
 
 if hash pocket-cli >/dev/null 2>&1; then
   pocket-cli add --url "$1" 2>&1 >/dev/null
